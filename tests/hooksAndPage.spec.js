3df38c97e5f812ab33563da5b24bd565899ccf1a
CJ <cjcpineda@gmail.com>
2025-10-14 15:01:45 -0400

    Section 8.2

--- tests/hooksAndPage.spec.js
+++ tests/hooksAndPage.spec.js
@@ -39,5 +39,26 @@ test.afterAll(async () => {
 test.only('A/B Test', async () => {
     await page.click('text="A/B Testing"');
     const header = await page.textContent('h3');
-    expect(header).toBe('A/B Test Control');
+    expect(header).toBe('A/B Test Variation 1');
+})
+
+test ('Checkbox verification', async () => {
+    await page.click('text=Checkboxes"');
+    const checkbox = await page.isChecked('input [type="checkbox"]:first-child');
+    expect (checkbox).toBe(false);
+})
+
+test.only ('Geolocation setting in context and verification', async () => {
+    context = await browser.newContext({
+        permissions: ['geolocation'],
+        geolocation: {latitude: 37.774929, longtitude:-122.419416}, //using san francisco lat long
+        viewport: {width: 1280, height: 720}
+    })
+    page = await context.newPage();
+    await page.pause();
+    await page.goto('https://the-internet.herokuapp.com/geolocation');
+    await page.click('button')
+    const lat =await page.textContent("#lat-value");
+    const long = await page.textContent("#long-value");
+    expect(latitude).
 })
